d9fa11bcc1bdcf898ebd97c07f2d374d63805ce4
David Jordânio Silveira <davidjvsilveira@gmail.com>
2023-07-19 00:04:13 -0300

    trabalhando na movimentação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,50 @@
-let painel = document.querySelector('.painel')
+let canvas; //Espaço no qual o jogo funcionará
+let ctx; //contexto, no caso 2D
+let dx = 5; //trava de variação de vel. horizontal do obj
+let dy = 5; //trava de variação de vel. horizontal do obj
+let x = 250; //posição horizontal inicial do obj
+let y = 100; //posição vertical inicial do obj
+let WIDTH = 600; //largura do painel
+let HEIGHT = 400; //altura do painel
 
-function criaCaixa(){
-    let caixa = document.createElement('div')
-    return caixa
+function desenhar(){
+    ctx.beginPath()
+    ctx.arch(x, y, 10, 0, Math.PI*2, true)
+    ctx.fill()
 }
-
-let linfocito = criaCaixa()
-painel.appendChild(linfocito)
-linfocito.classList.add('linfocito')
-
-let virus = criaCaixa()
-painel.appendChild(virus)
-virus.classList.add('virus')
-
+function limparTela(){
+    ctx.fillStyle = 'purple' //background do circulo
+    ctx.strokeStyle = 'black' //cor da borda
+    ctx.beginPath()
+    ctx.rect(0, 0, WIDTH, HEIGHT)
+    ctx.closePath()
+    ctx.fill() //desenho do circulo
+    ctx.stroke() //desenho da borda do circulo
+}
+function keyDown(e){ //definindo o começo da movimentação
+    switch(e.keyCode){
+        case 38: //seta para cima
+        if(y - dy > 0){ y -= dy;}
+        break;
+        case 40: //seta para baixo
+        if(y + dy < HEIGHT){ y += dy;}
+        break;
+        case 37: //seta para esquerda
+        if(x - dx > 0){ x -= dx;}
+        break;
+        case 39: //seta para direita
+        if(x + dx < WIDTH){ x += dx;}
+        break;
+    }
+}
+function atualizar(){
+    limparTela()
+    desenhar()
+}
+function iniciar(){
+    canvas = document.getElementById('canvas')
+    ctx = canvas.getContext('2d')
+    return setInterval(atualizar, 10)
+}
+window.addEventListener('keydown', keyDown,true)
+iniciar()
